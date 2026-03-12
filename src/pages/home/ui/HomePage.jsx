01912afbf88705e308c36fd56034078d233a2ae3
Bedu1441 <b.stoietskyi@gmail.com>
2026-03-12 19:58:26 -0400

    feat: add responsive Hero section with optimized images and shared Button component

--- src/pages/home/ui/HomePage.jsx
+++ src/pages/home/ui/HomePage.jsx
@@ -1,8 +1,10 @@
 import { Categories } from '@/features/categories/ui/Categories';
+import { Hero } from '@/widgets/layout/ui/Hero/Hero.jsx';
 
 export const HomePage = () => {
   return (
     <>
+      <Hero />
       <Categories />
     </>
   );
