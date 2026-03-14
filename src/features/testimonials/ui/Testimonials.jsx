04325e11ecbf370294d92239f75592f299f0c715
AlexSimakhin <oleksandrsimakhin@gmail.com>
2026-03-14 17:38:26 +0200

    feat: update Testimonials component styles and integrate into RecipesPage

--- src/features/testimonials/ui/Testimonials.jsx
+++ src/features/testimonials/ui/Testimonials.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
 import { useTestimonials } from '@/entities/testimonials/api/useTestimonials';
@@ -15,19 +14,19 @@ export const Testimonials = () => {
 
 	return (
     <section
-      className="container mt-[64px]"
+      className="container mt-16 tablet:mt-25 desktop:mt-30"
       aria-label="Testimonials"
     >
-      <p className="text-center text-sm tablet:text-base font-medium tracking-[-0.02em] mb-4">
+      <p className="text-center text-sm tablet:text-base font-medium tracking-[-0.02em] mb-4 text-dark">
         What our customers say
       </p>
 
-      <h2 className="text-center text-3xl tablet:text-[40px] tablet:leading-[44px] font-extrabold mb-3 tablet:-mb-2">
+      <h2 className="text-center text-3xl tablet:text-[40px] tablet:leading-11 font-extrabold mb-3 tablet:-mb-2">
         TESTIMONIALS
       </h2>
 
-      <div className="max-w-[822px] mx-auto">
-        <div className="mb-5 tablet:mb-10 pl-2 tablet:pl-10 text-[#BFBEBE]">
+      <div className="max-w-205.5 mx-auto">
+        <div className="mb-5 tablet:mb-10 pl-2 tablet:pl-10 text-grey">
           <svg width="59" height="48" aria-hidden>
             <use href="/icons.svg#quote-icon" />
           </svg>
@@ -52,7 +51,7 @@ export const Testimonials = () => {
               key={id}
               className="text-center"
             >
-              <p className="text-lg tablet:text-2xl mb-[64px] tablet:mb-[80px] font-medium">
+              <p className="text-lg tablet:text-2xl mb-16 tablet:mb-20 font-medium">
                 {testimonial}
               </p>
               <p className="uppercase tablet:text-xl font-bold mb-10">{name}</p>
--- src/pages/recipes/ui/RecipesPage.jsx
+++ src/pages/recipes/ui/RecipesPage.jsx
@@ -1,11 +1,13 @@
 import { Hero } from '@/widgets/layout/ui/Hero/Hero.jsx';
 import { Recipes } from '@/widgets/recipes/ui/Recipes';
+import { Testimonials } from '@/features/testimonials/ui/Testimonials';
 
 export const RecipesPage = () => {
   return (
     <>
       <Hero />
       <Recipes />
+      <Testimonials />
     </>
   );
 };
