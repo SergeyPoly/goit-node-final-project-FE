1d30add12dcc98225ffe814654852661519fdb32
serhiip-cpu <serhiip@paranagames.com>
2026-03-15 01:42:01 +0200

    minor subtitle style fix

--- src/pages/user/ui/UserPage.jsx
+++ src/pages/user/ui/UserPage.jsx
@@ -7,11 +7,11 @@ export const UserPage = () => {
   return (
     <section className="container">
       <MainTitle className="mt-8 tablet:mt-10 mb-4 tablet:mb-5">Profile</MainTitle>
-      <Subtitle className="mb-8 tablet:mb-10">
+      <Subtitle className="mb-8 tablet:mb-10 max-w-[443px]">
         Reveal your culinary art, share your favorite recipe and create gastronomic masterpieces with us.
       </Subtitle>
       <div className="flex flex-col desktop:flex-row gap-[64px] tablet:gap-[80px] desktop:gap-[40px]">
-        <div className="w-[394px] shrink-0">
+        <div className="tablet:w-[394px] mx-auto shrink-0">
           <Button variant="dark" className="w-full">
             logout
           </Button>
