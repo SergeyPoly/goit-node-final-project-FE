4a775516b6b02f4989744e9905060b253bbdf509
serhiip-cpu <serhiip@paranagames.com>
2026-03-16 00:42:53 +0200

    add husky to the project

--- src/pages/user/ui/UserPage.jsx
+++ src/pages/user/ui/UserPage.jsx
@@ -4,20 +4,18 @@ import { Subtitle } from '@/shared/ui/Subtitle.jsx';
 import { Button } from '@/shared/ui/Button.jsx';
 import { PathInfo } from '@/shared/ui/PathInfo.jsx';
 
-const pathItems = [
-  { label: 'Home', href: '/' },
-  { label: 'Profile' },
-]
+const pathItems = [{ label: 'Home', href: '/' }, { label: 'Profile' }];
 
 export const UserPage = () => {
   return (
     <section className="container">
       <PathInfo items={pathItems} />
-      <MainTitle className="tablet:mt-10 mb-4 tablet:mb-5">Profile</MainTitle>
-      <Subtitle className="mb-8 tablet:mb-10 max-w-[443px]">
-        Reveal your culinary art, share your favorite recipe and create gastronomic masterpieces with us.
+      <MainTitle className="tablet:mt-10 tablet:mb-5 mb-4">Profile</MainTitle>
+      <Subtitle className="tablet:mb-10 mb-8 max-w-[443px]">
+        Reveal your culinary art, share your favorite recipe and create gastronomic masterpieces
+        with us.
       </Subtitle>
-      <div className="flex flex-col desktop:flex-row gap-[64px] tablet:gap-[80px] desktop:gap-[40px]">
+      <div className="desktop:flex-row tablet:gap-[80px] desktop:gap-[40px] flex flex-col gap-[64px]">
         <div className="tablet:w-[394px] mx-auto shrink-0">
           <Button variant="dark" className="w-full">
             logout
